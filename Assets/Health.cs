577d1e19390c5ce45b85e3528cf7da30a863c54c
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-25 11:44:49 +0700

    Spawn and Initial

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -7,13 +7,22 @@ public class Health : NetworkBehaviour
 {
 
     public const int maxHealth = 100;
-
     public bool destroyOnDeath;
 
     [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
 
     public RectTransform healthBar;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
 
     public void TakeDamage(int amount)
     {
@@ -31,7 +40,7 @@ public class Health : NetworkBehaviour
             {
                 currentHealth = maxHealth;
 
-                // called on the Server, will be invoked on the Clients
+                // called on the Server, invoked on the Clients
                 RpcRespawn();
             }
         }
@@ -47,8 +56,17 @@ public class Health : NetworkBehaviour
     {
         if (isLocalPlayer)
         {
-            // Set the player’s position to origin
-            transform.position = Vector3.zero;
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick one at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player’s position to the chosen spawn point
+            transform.position = spawnPoint;
         }
     }
 }
